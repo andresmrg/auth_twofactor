83a24195df2bc3551cd8ec4358d65763ded3ec5a
Andres Ramos <andresmao2@gmail.com>
2018-03-18 18:12:27 -0500

    Remove debugging options.

--- confirm.php
+++ confirm.php
@@ -27,10 +27,10 @@ require_once('../../config.php');
 require_once('confirm_form.php');
 require 'vendor/autoload.php';
 
-@error_reporting(E_ALL | E_STRICT); // NOT FOR PRODUCTION SERVERS!
-@ini_set('display_errors', '1');    // NOT FOR PRODUCTION SERVERS!
-$CFG->debug = (E_ALL | E_STRICT);   // === DEBUG_DEVELOPER - NOT FOR PRODUCTION SERVERS!
-$CFG->debugdisplay = 1;             // NOT FOR PRODUCTION SERVERS!
+// @error_reporting(E_ALL | E_STRICT); // NOT FOR PRODUCTION SERVERS!
+// @ini_set('display_errors', '1');    // NOT FOR PRODUCTION SERVERS!
+// $CFG->debug = (E_ALL | E_STRICT);   // === DEBUG_DEVELOPER - NOT FOR PRODUCTION SERVERS!
+// $CFG->debugdisplay = 1;             // NOT FOR PRODUCTION SERVERS!
 
 // Get url params.
 $accesskey = get_config('auth_twofactor', 'accesskey');
